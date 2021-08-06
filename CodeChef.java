605ffc9669d3e154a22868a828b686fd8495748e
Anil Kumar <75405903+code-repeat-code@users.noreply.github.com>
2021-08-06 08:58:37 +0530

    Revert "Revert "Revert "GCD and LCM """
    This reverts commit abcdd8509edb5edd9287b0c7adf904752639731f.

--- CodeChef.java
+++ CodeChef.java
@@ -1,34 +1,36 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-	public static long gcd(long x,long y)
-	{
-	    if(x==0)
-	    return y;
-	    else
-	    return gcd(y%x,x);
-	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t-->0)
+		try 
 		{
-		    long a = sc.nextInt();
-		    long b = sc.nextInt();
-		    //gcd lcm
-		    long gc = gcd(a,b);
-		    long lcm = (a*b)/gc;
-		    System.out.println(gc+" "+lcm);
+		    int t = sc.nextInt();
+		    while(t-->0)
+		    {
+		        int x1,x2,y1,y2,z1,z2;
+		        x1 = sc.nextInt();
+		        x2 = sc.nextInt();
+		        y1 = sc.nextInt();
+		        y2 = sc.nextInt();
+		        z1 = sc.nextInt();
+		        z2 = sc.nextInt();
+		        if(x2>=x1 && y2>=y1 && z1>=z2)
+		        {
+		            System.out.println("YES");
+		        }
+		        else
+		        {
+		            System.out.println("NO");
+		        }
+		    }
 		    
-		    
+		} catch(Exception e) {
+		    return;
 		}
 	}
 }
